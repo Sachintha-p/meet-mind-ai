2eae0a0d1b005e528d6d0338597729090b702068
Sachintha Praneeth <it23595262@my.sliit.lk>
2025-08-14 01:18:13 +0530

    module begin

---
+++ src/app/auth/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import { Card } from "@/components/ui/card";
+
+const Page = () => {
+    return (
+        <div>
+            <Card>
+            Sign up page
+            </Card>
+        </div>
+    );
+}
+export default Page;
