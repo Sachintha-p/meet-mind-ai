66d8081a882009742e198db9c8d7e8274c90b6cb
Sachintha Praneeth <it23595262@my.sliit.lk>
2025-08-14 01:18:50 +0530

    module begin

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,14 +1,6 @@
-import { Card } from "@/components/ui/card";
+import { SignInView } from "@/components/ui/card";
 
 const Page = () => {
-    return (
-        <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-            <div className="w-full max-w-sm md:max-w-3xl">
-                <Card>
-                 Sign in page
-                </Card>
-        </div>
-        </div>
-    );
+    return<SignInView/>
 }
 export default Page;
---
+++ src/modules/auth/ui/views/sign-inview.tsx
@@ -0,0 +1,9 @@
+import {Card} from "@components/ui/card"
+
+export const SignInView = () => {
+    return(
+        <Card>
+            Sign in view
+        </Card>
+    )
+}
