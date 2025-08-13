35798038ef159e5d24d42a4d3a2f40d1c1fc2b71
Sachintha Praneeth <it23595262@my.sliit.lk>
2025-08-13 23:51:02 +0530

    authanticate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,6 +28,20 @@ export default function Home() {
 
       });
   }
+    const onLogin = () => {
+        authClient.signIn.email({
+            email,
+            password,
+        },{
+            onError: () => {
+                window.alert("Something went wrong")
+            },
+            onSuccess: () => {
+                window.alert("Success")
+            }
+
+        });
+    }
 
   if (session){
       return(
@@ -41,7 +55,8 @@ export default function Home() {
   }
 
 return(
-   <div className="p-4 flex flex-col gap-y-4">
+    <div className="flex flex-col p-4 gap-y-10">
+    <div className="p-4 flex flex-col gap-y-4">
      <Input
          placeholder="name"
          value={name}
@@ -60,6 +75,22 @@ return(
      <Button onClick={onSubmit}>
        Create user
      </Button>
-   </div>
+    </div>
+        <div className="p-4 flex flex-col gap-y-4">
+            <Input
+                placeholder="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+                placeholder="password" type='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button onClick={onLogin}>
+                Login
+            </Button>
+        </div>
+    </div>
 );
 };
